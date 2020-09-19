92d2740c8ef3480dbcbd9e8505df3f2ad9261c19
vitor <vitorgabriel.unb@gmail.com>
2020-09-18 21:46:34 -0300

    chapter 6 adicionado profile page, Avatars, e edicao de profile

--- app/models.py
+++ app/models.py
@@ -2,6 +2,7 @@ from app import db, login
 from datetime import datetime
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
+from hashlib import md5
 
 
 class User(UserMixin, db.Model):
@@ -9,6 +10,8 @@ class User(UserMixin, db.Model):
 	username = db.Column(db.String(64), index=True, unique=True)
 	email = db.Column(db.String(120), index=True, unique=True)
 	password_hash = db.Column(db.String(128))
+	about_me = db.Column(db.String(140))
+	last_seen = db.Column(db.DateTime, default=datetime.utcnow)
 	posts = db.relationship('Post', backref='author', lazy='dynamic')
 
 	def __repr__(self):
@@ -23,6 +26,10 @@ class User(UserMixin, db.Model):
 	@login.user_loader
 	def load_user(id):
 		return User.query.get(int(id))
+
+	def avatar(self, size):
+		digest = md5(self.email.lower().encode('utf-8')).hexdigest()
+		return f'https://www.gravatar.com/avatar/{digest}?d=identicon&s={size}'
 
 
 class Post(db.Model):
